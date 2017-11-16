a65b254d6d01bee036bfd3fa4feebde1c7974a19
Kmilo1488 <camilomoreno1488@live.com>
2017-11-15 21:55:11 -0500

    add function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor(props){
+    super(props);
+    this.incrementa = this.incrementa.bind(this)
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementa(){
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.incrementa}>Incrementa</button>
       </div>
     );
   }
